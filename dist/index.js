dce6f43bb5179a7b68e6f3743f26ed8477bac961
valkyrie_pilot <valk@randomairborne.dev>
2025-07-04 10:35:22 -0600

    trigger for regeneration

--- dist/index.js
+++ dist/index.js
@@ -27260,23 +27260,42 @@ function requireSrc () {
 	const fs = require$$1$6;
 
 	async function main() {
-	  const file = core.getInput("file", { required: true });
-	  const upload_to = core.getInput("upload_url", { required: true });
+	  const file = core.getInput("file");
+	  const upload_to = core.getInput("upload_url");
+	  const regenerate_with = core.getInput("regenerate_url");
 	  const id_token = await core.getIDToken();
 
-	  const body = await fs.readFile(file);
+	  if (file) {
+	    if (!upload_to) {
+	      throw "upload_to must be set if file is set";
+	    }
+	    const body = await fs.readFile(file);
+	    const upload = await fetch(upload_to, {
+	      method: "POST",
+	      headers: {
+	        "openid-token": id_token,
+	      },
+	      body: body,
+	    });
+	    if (upload.status !== 200) {
+	      const bytes = await upload.bytes();
+	      const bytesStr = new TextDecoder().decode(bytes);
+	      throw `Status was not 200: ${bytesStr}`;
+	    }
+	  }
 
-	  const resp = await fetch(upload_to, {
-	    method: "POST",
-	    headers: {
-	      "openid-token": id_token,
-	    },
-	    body: body,
-	  });
-	  if (resp.status !== 200) {
-	    const bytes = await resp.bytes();
-	    const bytesStr = bytes.toString();
-	    throw `Status was not 200: ${bytesStr}`;
+	  if (regenerate_with) {
+	    const regen = await fetch(regenerate_with, {
+	      method: "POST",
+	      headers: {
+	        "openid-token": id_token_a,
+	      },
+	    });
+	    if (regen.status !== 200) {
+	      const bytes = await resp.bytes();
+	      const bytesStr = bytes.toLocaleString();
+	      throw `Status was not 200: ${bytesStr}`;
+	    }
 	  }
 	}
 
